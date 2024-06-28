ce84df6d38057bc69ff9b11171074ccd0720affe
chasemao <chase-mao@outlook.com>
2024-06-28 23:48:39 +0800

    linkedin

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -47,6 +47,9 @@ function ArticalList() {
         ))}
       </div>
       <div className="App-introduction">
+          <a href="https://www.linkedin.com/in/chase-mao" target="_blank">
+            <img src="/linkedin.ico" style={{ width: '20px', height: '20px'}} />
+          </a>
         {introduction}
       </div>
     </div>
